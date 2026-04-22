a8f806a9fea45aea6df2230d3f78b3e18dda6851
chaima hizaoui <chaima.hizaoui@isamm.u-manouba.tn>
2026-04-22 17:50:35 +0100

    correction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Contracts\StatusStrategyInterface;
+use App\Services\Strategies\ActiveInactiveStrategy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(StatusStrategyInterface::class, ActiveInactiveStrategy::class);
     }
 
     /**
